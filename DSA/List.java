b7445bf08e17b597e130288088f6d0172fb33042
mahi <230150274+themahikaushik@users.noreply.github.com>
2026-02-17 10:38:54 +0530

    ,,

--- DSA/List.java
+++ DSA/List.java
@@ -1,9 +1,10 @@
 package DSA;
 
-public class List {
+public class  {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 List<String>fruits=new ArrayList<>();
+	 
 
 	}
 
